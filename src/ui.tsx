03099e2b9d359369a29f6e87d6cc2178cd17b325
Luke Dowling <lpdowling26@googlemail.com>
2026-01-31 15:04:14 +0100

    add health to ui bar

--- src/ui.tsx
+++ src/ui.tsx
@@ -8,15 +8,15 @@ export default function UI() {
   const maxHealth = 5;
 
   const healthStates = Array.from({ length: maxHealth }, (_, index) =>
-    index < playerHealth ? "healthy" : "damaged",
+    index < playerHealth ? "healthy" : "damaged"
   );
 
   return (
     <UIElement>
       <div className="flex justify-between text-4xl text-black">
         <div className="border rounded-lg bg-white flex flex-col p-4 m-4">
-          <p>Level: 9</p>
-          <p>Score: 9000</p>
+          <p>Level: 1</p>
+          <p>Health: {playerHealth}</p>
         </div>
         <div className="border rounded-lg bg-white flex p-4 m-4 gap-2 items-center">
           {healthStates.map((state, index) => (
